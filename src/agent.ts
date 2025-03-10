9026027200f0a5532ea2e378d46713a05b37ecac
ssamaresh <ssamaresh1990@gmail.com>
2025-03-09 22:59:08 -0400

    Added Agent loop

--- src/agent.ts
+++ src/agent.ts
@@ -14,22 +14,26 @@ export const runAgent = async ({
   await addMessages([{ role: 'user', content: userMessage }])
 
   const loader = showLoader('🤔')
-  const history = await getMessages()
+  while (true) {
+    const history = await getMessages()
 
-  const response = await runLLM({ messages: history, tools })
-  await addMessages([response])
+    const response = await runLLM({ messages: history, tools })
+    await addMessages([response])
 
-  if (response.tool_calls) {
-    const toolCall = response.tool_calls[0]
+    if (response?.content) {
+      loader.stop()
+      logMessage(response);
+      return getMessages()
+    }
 
-    loader.update(`executing: ${toolCall.function.name}`)
+    if (response.tool_calls) {
+      const toolCall = response.tool_calls[0]
+      logMessage(response);
+      loader.update(`executing: ${toolCall.function.name}`)
 
-    const toolResponse = await runTool(toolCall, userMessage)
-    await saveToolResponse(toolCall.id, toolResponse)
-    loader.update(`done: ${toolCall.function.name}`)
+      const toolResponse = await runTool(toolCall, userMessage)
+      await saveToolResponse(toolCall.id, toolResponse)
+      loader.update(`done: ${toolCall.function.name}`)
+    }
   }
-
-  logMessage(response)
-  loader.stop()
-  return getMessages()
 }
